653b01b5ba25c070bdb932065c54164c39f9331b
Your Name <you@example.com>
2024-10-30 15:36:39 +0700

    first commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,9 +4,26 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        const a = this[i];
+        const b = this[j];
+        if (compareFunction(a, b) < 0) {
+          continue;
+        } else {
+          const tmp = this[i];
+          this[i] = this[j];
+          this[j] = tmp;
+        }
+      }
+    }
+    return this;
   };
 }
+
+// applyCustomSort();
+// const result = [3, 2, 1].sort2((a, b) => a - b);
+// console.log(result); // Output: [1, 2, 3]
 
 module.exports = applyCustomSort;
