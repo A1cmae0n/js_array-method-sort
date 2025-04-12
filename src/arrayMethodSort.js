8d16f639aa37dc58ca6e2ee81faf4b16a9817c7f
Your Name <you@example.com>
2025-04-12 14:06:34 +0700

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,8 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    // Default comparison function for strings
+    let callback;
+    if (typeof compareFunction === 'undefined') {
+      callback = (a, b) => String(a).localeCompare(String(b));
+    } else {
+      callback = compareFunction;
+    }
+    // Bubble sort implementation
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        // If callback returns positive value, swap elements
+        if (callback(this[j], this[j + 1]) > 0) {
+          // Swap elements
+          const temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+    return this;
   };
 }
 
 module.exports = applyCustomSort;
+
+applyCustomSort();
